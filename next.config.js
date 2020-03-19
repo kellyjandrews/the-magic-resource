328f37d4a0ede5f8263061eb648c7aae8d692eac
Kelly J Andrews <kelly@kellyjandrews.com>
2020-03-19 17:37:45 -0400

    config updates

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
-const webpack = require("webpack");
-require("dotenv").config();
-
 module.exports = {
-  webpack: config => {
-    config.node = {
-      fs: 'empty'
-    }
-
-    const env = Object.keys(process.env).reduce((acc, curr) => {
-      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
-      return acc;
-    }, {});
-
-    config.plugins.push(new webpack.DefinePlugin(env));
-
-    return config
+  env: {
+    GHOST_URL: process.env.GHOST_URL,
+    GHOST_API_KEY: process.env.GHOST_API_KEY,
   }
-};
+}

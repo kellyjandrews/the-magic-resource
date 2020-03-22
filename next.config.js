24658a93a8ccc2fed5f9a2574fcad664c0384ae5
Kelly J Andrews <kelly@kellyjandrews.com>
2020-03-22 17:28:35 -0400

    config updates

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+const withCSS = require('@zeit/next-css');
+require('dotenv').config();
+
+module.exports = withCSS({
   env: {
     GHOST_URL: process.env.GHOST_URL,
     GHOST_API_KEY: process.env.GHOST_API_KEY,
   }
-}
+});

2637842b08bd74189fcbee9715e91631678069b2
Kelly J Andrews <kelly@kellyjandrews.com>
2020-03-22 17:31:07 -0400

    add portion of new design

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className="flex items-center justify-between flex-wrap bg-white p-6">
+      <h1><a href="/">the magic resource</a></h1>
+    </nav>
+  )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { getPosts } from '../api/posts';
-
+import { Box, Button, Grid, Image, Link } from "@chakra-ui/core";
 
 const Home = () => {
   const [posts, setPosts] = useState();
@@ -14,14 +14,58 @@ const Home = () => {
     }
 
     getPostList();
+
   }, [])
 
   return (
-    <ul>
+    <Grid templateColumns="repeat(4, 1fr)" gap={12}>
       {posts?.map(post => (
-        <li key={post.id}><a href={`${post.slug}`}>{post.title}</a></li>
-      ))}
-    </ul>
+        <Box
+          d="flex"
+          flexDir="column"
+          maxW="sm"
+          boxShadow="5px 5px 10px #888888"
+          borderWidth="1px"
+          rounded="lg"
+          overflow="hidden"
+        >
+          <Box
+            p="10"
+          >
+            <Image
+              src={post.feature_image}
+              alt={post.title}
+              boxShadow="5px 5px 10px #888888"
+            />
+          </Box>
+          <Box
+              p="5"
+              fontWeight="semibold"
+              as="h3"
+              isTruncated
+            >
+              {post.title}
+          </Box>
+          <Box
+            p="5"
+            fontWeight="normal"
+            as="p"
+          >
+            {post.excerpt}
+          </Box>
+          <Box
+            p="5"
+            alignSelf="flex-end"
+            mt="auto"
+          >
+            <Link href={`/${post.slug}`}>
+              <Button variantColor="green">Read More</Button>
+            </Link>            
+          </Box>
+        </Box>
+    ))}
+    </Grid>
+        
   )
 }
 

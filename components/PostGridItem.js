721c571886707583ee6b0fee883063c57aef657a
Kelly J Andrews <kelly@kellyjandrews.com>
2020-04-01 23:17:17 -0400

    clean up grid

--- components/PostGridItem.js
+++ components/PostGridItem.js
@@ -1,44 +1,39 @@
-import { Box, Button, Image, Link } from "@chakra-ui/core"; 
+import moment from 'moment';
+import { Box, Button, Image, Heading, Link, Text } from "@chakra-ui/core";
 
-const PostGridItem = ({ feature_image, published_at, slug, title}) => {
+const PostGridItem = ({ feature_image, published_at, slug, title }) => {
   return (
-    <Box
-      d="flex"
-      flexDir="column"
-      maxW={{ sm: "xs" }}
-      boxShadow="5px 5px 10px #888888"
-      rounded="sm"
-      overflow="hidden"
-      bg="white"
-    >
+    <Link href={`/${slug}`}>
+
       <Box
-        p="10"
-        bg="grey"
+        d="flex"
+        flexDir="column"
+        maxW={{ sm: "xs" }}
+        boxShadow="5px 5px 10px #888888"
+        rounded="sm"
+        overflow="hidden"
+        bg="white"
       >
-        <Image
-          src={feature_image}
-          alt={title}
-          boxShadow="5px 5px 10px #888888"
-        />
+        <Box
+          p="10"
+          bg="grey"
+        >
+          <Image
+            src={feature_image}
+            alt={title}
+            boxShadow="5px 5px 10px #888888"
+          />
+        </Box>
+        <Box
+          p="5"
+          fontWeight="semibold"
+          isTruncated
+        >
+          <Heading as="h4" size="md">{title}</Heading>
+          <Text fontSize="xs"><em>Published</em>: {moment(published_at).format('MMMM, Do YY')}</Text>
+        </Box>
       </Box>
-      <Box
-        p="5"
-        fontWeight="semibold"
-        as="h3"
-        isTruncated
-      >
-        {title}
-      </Box>
-      <Box
-        p="5"
-        alignSelf="flex-end"
-        mt="auto"
-      >
-        <Link href={`/${slug}`}>
-          <Button variantColor="green">Read More</Button>
-        </Link>
-      </Box>
-    </Box>
+    </Link>
   );
 }
 

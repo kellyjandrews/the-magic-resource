68965c787f97a81d1a019e958e07cff115f79d11
Kelly J Andrews <kelly@kellyjandrews.com>
2020-04-01 23:17:48 -0400

    slider clean up work

--- components/FeaturedPosts/FeaturedPostItem.js
+++ components/FeaturedPosts/FeaturedPostItem.js
@@ -2,18 +2,18 @@ import { AspectRatioBox, Box, Heading, Image, Link } from "@chakra-ui/core";
 
 const FeaturedPostItem = ({ feature_image, slug, title }) => {
   return (
-    <Box position={"relative"} w={"100%"} className="slide">
+    <Link href={`/${slug}`} w={"100%"} className="slide">
+      <Box position={"relative"} >
         <AspectRatioBox maxH={"400px"}>
           <Image src={feature_image} objectFit="cover" />
         </AspectRatioBox>
-        <Link href={`/${slug}`}>
-          <Box bg={"white"} maxW={"25%"} boxShadow="5px 5px 10px #888888" p={"6"} position={"absolute"} zIndex={2} top="0" left="0">
-            <Heading as="h1" size="2xl">
-              {title}
-            </Heading>
-          </Box>
-        </Link>
+        <Box bg={"white"} boxShadow="5px 5px 10px #888888" p={"6"} position={"absolute"} zIndex={2} bottom="0" >
+          <Heading as="h1" size="2xl">
+            {title}
+          </Heading>
+        </Box>
       </Box>
+    </Link>
   )
 };
 
--- components/FeaturedPosts/FeaturedPostSlider.js
+++ components/FeaturedPosts/FeaturedPostSlider.js
@@ -3,47 +3,43 @@ import FeaturedPostItem from './FeaturedPostItem';
 import { Box, Icon } from "@chakra-ui/core";
 import "./FeaturedPostSlider.css";
 
-function lastToFirst(arr) {
-  let lastItem = arr[arr.length - 1]
-  return [lastItem, ...arr.slice(0, arr.length - 1)];
-};
-
-function firstToLast(arr) {
-  let firstItem = arr[0];
-  return [...arr.slice(1), firstItem];
-};
-
-
 const FeaturedPostSlider = ({ featureList = [], interval = 5000, slidesPresented = 1 }) => {
   const length = featureList.length;
   const numActive = Math.min(length, slidesPresented);
   const [active, setActive, handlers, style] = useCarousel(length, interval, { slidesPresented: numActive });
-
+  
   return (
     <Box
       className="slider"
       w="100%"
       mb={6}
       position="relative"
+      display="flex"
+      flexDirection="column"
       overflow="hidden"
+      alignContent="center"
     >
       <Box
         className="slider-container"
         display="flex"
         flexDirection="row"
+        pb={6}
         minH={"400px"}
         overflow="hidden"
         position={"relative"}
         style={style}
         {...handlers}
       >
-        <FeaturedPostItem {...featureList[featureList.length-1]} />
+        <FeaturedPostItem {...featureList[featureList.length - 1]} />
         {featureList?.map((feature, index) => <FeaturedPostItem {...feature} id={index} key={feature.id} />)}
         <FeaturedPostItem {...featureList[0]} />
       </Box>
-      <Icon name="chevron-left" focusable size={20} position="absolute" top="43%" left="0" zIndex={3} />
-      <Icon name="chevron-right" focusable size={20} position="absolute" top="43%" right="0" zIndex={3} />
-      {featureList?.map((feature, index) => <span key={index} onClick={() => setActive(index)} className={`dot ${active === index ? "active" : ""}`} />)}
+      <Icon name="chevron-left" onClick={() => active === 0 ? setActive(featureList.length) : setActive(active - 1)} focusable size={20} position="absolute" top="35%" left="0" zIndex={3} />
+      <Icon name="chevron-right"
+        onClick={() => active === featureList.length - 1 ? setActive(0) : setActive(active + 1)} focusable size={20} position="absolute" top="35%" right="0" zIndex={3} />
+      <Box textAlign="center">
+        {featureList?.map((_, index) => <span key={index} onClick={() => setActive(index)} className={`dot ${active === index ? "active" : ""}`} />)}
+      </Box>
     </Box>
   );
 }
